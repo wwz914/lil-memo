fe44bd375eb84036f8167cf01d8fd82c07e99f01
wwz914 <3262961369@qq.com>
2024-02-15 22:37:41 +0800

    pages updated

---
+++ pages/muser/maccount.js
@@ -0,0 +1,17 @@
+Component({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 3
+        })
+      }
+    }
+  },
+  methods:{
+    logout(){
+      console.log('注销');
+    }
+  }
+})
--- pages/muser/muser.js
+++ pages/muser/muser.js
@@ -1,4 +1,7 @@
 Component({
+  data:{
+    dialog:false,
+  },
   pageLifetimes: {
     show() {
       if (typeof this.getTabBar === 'function' &&
@@ -7,6 +10,26 @@ Component({
           selected: 3
         })
       }
+    }
+  },
+  methods:{
+    // 跳转至账号页面
+    toAccount(){
+      wx.switchTab({
+        url: '/pages/muser/maccount'
+      })
+    },
+    // 打开加密弹窗
+    toDialog(){
+      this.setData({
+        dialog:true,
+      })
+    },
+    // 关闭加密弹窗
+    closeDialog(){
+      this.setData({
+        dialog:false,
+      })
     }
   }
 })
